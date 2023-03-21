179f445b84385c01d0dfcead7cc84655f4923c03
piotr-chmura <piotr.chmura17@gmail.com>
2023-03-21 21:05:36 +0100

    If jakby nie było dostępu do bazy

--- lib/backend/loginBackend.dart
+++ lib/backend/loginBackend.dart
@@ -6,31 +6,34 @@ import 'database.dart';
 class LoginDAO extends DAO {
   Future<String> loginUser(User user) async {
     String error = "";
-    await db!.getConn().then((conn) async {
-      String sql = 'select password from t_users where nickname = ? limit 1;';
-      await conn.connect();
-      var prepareStatment = await conn.prepare(sql);
-      await prepareStatment.execute([user.username]).then((result) {
-        if (result.numOfRows == 1) {
-          for (var row in result.rows) {
-            String? dbPassword = encrypter
-                ?.decrypt64(row.colAt(0).toString(), iv: iv)
-                .toString();
-            String dbPasswordValidate = dbPassword ?? "";
-            String passwordValidate = user.password ?? "";
-            if (isEven(dbPasswordValidate, passwordValidate)) {
-              error = "Nie poprawna nazwa użytkownika lub hasło";
+    //tymczasowy if do testowania jak nie będzie dostępu do bazy
+    if (user.username != "admin1" || user.password != "admin1") {
+      await db!.getConn().then((conn) async {
+        String sql = 'select password from t_users where nickname = ? limit 1;';
+        await conn.connect();
+        var prepareStatment = await conn.prepare(sql);
+        await prepareStatment.execute([user.username]).then((result) {
+          if (result.numOfRows == 1) {
+            for (var row in result.rows) {
+              String? dbPassword = encrypter
+                  ?.decrypt64(row.colAt(0).toString(), iv: iv)
+                  .toString();
+              String dbPasswordValidate = dbPassword ?? "";
+              String passwordValidate = user.password ?? "";
+              if (isEven(dbPasswordValidate, passwordValidate)) {
+                error = "Nie poprawna nazwa użytkownika lub hasło";
+              }
             }
+          } else {
+            error = "Nie poprawna nazwa użytkownika lub hasło";
           }
-        } else {
-          error = "Nie poprawna nazwa użytkownika lub hasło";
-        }
-      }, onError: (details) {
-        error = details.toString();
+        }, onError: (details) {
+          error = details.toString();
+        });
+        await prepareStatment.deallocate();
+        await conn.close();
       });
-      await prepareStatment.deallocate();
-      await conn.close();
-    });
+    }
     return error;
   }
 }
