199733954bf3c25b9def410bcbbc35eb3f145fb5
mati1r <mati1r@wp.pl>
2023-04-09 14:22:54 +0200

    to samo do maina

--- lib/mainMenu.dart
+++ lib/mainMenu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'betMatch.dart';
 import 'sideMenu.dart';
 
 class MainMenu extends StatefulWidget {
@@ -26,21 +27,64 @@ class _MainMenu extends State<MainMenu> {
     super.initState();
     getUsername();
   }
+  Future<void> _navigateAndDisplaySelection(BuildContext context, int match_id,
+        String groupName, teamA, teamB, data) async {
+      final result = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => BetMatch(
+                match_id: match_id,
+                groupName: groupName,
+                teamA: teamA,
+                teamB: teamB,
+                data: data)),
+      );
 
+      if (!mounted) return;
+      if (result == null) return;
+
+      ScaffoldMessenger.of(context)
+        ..removeCurrentSnackBar()
+        ..showSnackBar(SnackBar(
+            content: Center(
+              child: Text('$result', style: const TextStyle(color: Colors.green)),
+            ),
+            duration: const Duration(milliseconds: 3000),
+            behavior: SnackBarBehavior.floating,
+            backgroundColor: const Color.fromARGB(255, 66, 66, 66)));
+    }
 
   Widget matches() {
     return Column(children: <Widget>[
-      mecz("Bundesliga", "26.02.2023, 16:15", "Borussia Dortmund", "Bayer Leverkusen"),
-      mecz("Seria A", "27.02.2023, 20:10", "Inter", "Juventus")
+      mecz("Bundesliga", "26.02.2023, 16:15", "Borussia Dortmund", "Bayer Leverkusen", 1, "Zakonczony"),
+      mecz("Seria A", "27.02.2023, 20:10", "Inter", "Juventus", 2, "Dostepny")
     ]);
   }
 
-  GestureDetector mecz(groupName, data, teamA, teamB) {
+  GestureDetector mecz(groupName, data, teamA, teamB, match_id, status) {
+    Color T_color =  Color.fromRGBO(20, 150, 37, 1);
+    if(status == "Zakonczony"){
+      T_color = Color.fromRGBO(140, 15, 15, 1);
+    }else if(status == "W trakcie"){
+      T_color =Color.fromRGBO(100, 100, 100, 1);
+    }
     return GestureDetector(
         onTap: () {
-          if (kDebugMode) {
-            print("Check");
-          }
+          if(status == "Dostepny"){
+            _navigateAndDisplaySelection(
+                context, match_id, groupName, teamA, teamB, data);
+            }
+            else{
+              ScaffoldMessenger.of(context)
+              ..removeCurrentSnackBar()
+              ..showSnackBar(const SnackBar(
+              content: Center(
+                child: Text("Brak możliwości typowania meczu w trakcie lub zakończonego", style: TextStyle(color: Colors.green)),
+              ),
+              duration: Duration(milliseconds: 3000),
+              behavior: SnackBarBehavior.floating,
+              backgroundColor: Color.fromARGB(255, 66, 66, 66)));
+            }
         },
         child: Container(
             alignment: Alignment.center,
@@ -48,7 +92,7 @@ class _MainMenu extends State<MainMenu> {
             margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
             decoration: BoxDecoration(
                 border: Border.all(
-                    width: 5, color: const Color.fromRGBO(100, 100, 100, 1)),
+                    width: 5, color: T_color),
                 borderRadius: const BorderRadius.all(Radius.circular(10))),
             child: Column(
               children: <Widget>[
