a4a4271593cdb7b8987476ebbeb93d0efe4025d8
piotr-chmura <piotr.chmura17@gmail.com>
2023-04-14 14:19:30 +0200

    PCH

--- lib/backend/groupsBackend.dart
+++ lib/backend/groupsBackend.dart
@@ -14,8 +14,46 @@ class GroupDAO extends DAO {
                       INNER JOIN t_groups_users gu
                       ON g.id_group = gu.group_id_group
                       INNER JOIN t_users u
-                      ON gu.user_id_user = u.id_user
+                      ON gu.user_id_user = u.id_user 
                       WHERE g.id_group NOT IN
+                        (
+                          SELECT group_id_group
+                          FROM t_groups_users
+                          WHERE user_id_user = ?
+                          );'''; //ADMIN?
+      await conn.connect();
+      var prepareStatment = await conn.prepare(sql);
+      await prepareStatment.execute([idUser]).then((result) {
+        if (result.numOfRows > 0) {
+          for (var row in result.rows) {
+            groups.add(Group(int.parse(row.colAt(0) ?? "error"), row.colAt(1),
+                row.colAt(2)));
+          }
+        } else {
+          throw Exception(
+              "Błąd bazy danych: Użytkownik należy do wszystkich grup");
+        }
+      }, onError: (details) {
+        throw Exception(details.toString());
+      });
+      await prepareStatment.deallocate();
+      await conn.close();
+    });
+    return groups;
+  }
+
+  Future<List<Group>> userGroupList() async {
+    List<Group> groups = [];
+    final prefs = await SharedPreferences.getInstance();
+    final idUser = prefs.getString('id');
+    await db!.getConn().then((conn) async {
+      String sql = '''SELECT g.id_group, g.name, u.nickname
+                      FROM t_groups g
+                      INNER JOIN t_groups_users gu
+                      ON g.id_group = gu.group_id_group
+                      INNER JOIN t_users u
+                      ON gu.user_id_user = u.id_user
+                      WHERE g.id_group IN
                         (
                           SELECT group_id_group
                           FROM t_groups_users
@@ -31,7 +69,7 @@ class GroupDAO extends DAO {
           }
         } else {
           throw Exception(
-              "Błąd bazy danych: Użytkownik należy do wszystkich grup");
+              "Błąd bazy danych: Użytkownik nie należy do żadnej grupy");
         }
       }, onError: (details) {
         throw Exception(details.toString());
