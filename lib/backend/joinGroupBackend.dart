d8c2d1fe0635fe3e52ec0846a240849ce226bd85
piotr-chmura <piotr.chmura17@gmail.com>
2023-06-02 13:53:11 +0200

    Anty spam i logo hamburger

--- lib/backend/joinGroupBackend.dart
+++ lib/backend/joinGroupBackend.dart
@@ -7,14 +7,33 @@ class JoinGroupDAO extends DAO {
     final prefs = await SharedPreferences.getInstance();
     final idUser = prefs.getString('id');
     await db!.getConn().then((conn) async {
-      String sql = 'insert into t_groups_users values (?, ?, 0);';
+      String sql1 =
+          'SELECT COUNT(*) FROM t_groups_users WHERE user_id_user = ?;';
       await conn.connect();
-      var prepareStatment = await conn.prepare(sql);
-      await prepareStatment.execute([idGroup, idUser]).then((result) {},
-          onError: (details) {
+      var prepareStatment = await conn.prepare(sql1);
+      await prepareStatment.execute([idUser]).then((result) {
+        if (result.numOfRows > 0) {
+          for (var row in result.rows) {
+            if (int.parse(row.colAt(0)!) >= 10) {
+              throw Exception(
+                  "Użytkownik należy już do maksymalnej ilości grup czyli 10");
+            }
+          }
+        } else {
+          throw Exception("Błąd bazy danych");
+        }
+      }, onError: (details) {
         throw Exception(details.toString());
       });
       await prepareStatment.deallocate();
+
+      String sql2 = 'insert into t_groups_users values (?, ?, 0);';
+      var prepareStatment2 = await conn.prepare(sql2);
+      await prepareStatment2.execute([idGroup, idUser]).then((result) {},
+          onError: (details) {
+        throw Exception(details.toString());
+      });
+      await prepareStatment2.deallocate();
       await conn.close();
     });
   }
--- lib/joinGroup.dart
+++ lib/joinGroup.dart
@@ -53,12 +53,9 @@ class _JoinGroup extends State<JoinGroup> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Container(
-              margin: const EdgeInsets.fromLTRB(0, 0, 50, 0),
-              child: const Center(child: Text('Typster')
-              )
-          )
-        ),
+            title: Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+                child: const Center(child: Text('Typster')))),
         body: Padding(
             padding: const EdgeInsets.fromLTRB(10, 30, 10, 20),
             child: Center(
@@ -68,36 +65,29 @@ class _JoinGroup extends State<JoinGroup> {
                   "Czy chcesz dołączyć do grupy:",
                   style: TextStyle(fontSize: 20),
                 ),
-                const Padding(padding: EdgeInsets.fromLTRB(10, 30, 10, 20)
-                ),
+                const Padding(padding: EdgeInsets.fromLTRB(10, 30, 10, 20)),
                 Text(
                   widget.groupName,
                   style: const TextStyle(color: Colors.green, fontSize: 20),
                 ),
-                const Padding(padding: EdgeInsets.fromLTRB(10, 30, 10, 20)
-                ),
+                const Padding(padding: EdgeInsets.fromLTRB(10, 30, 10, 20)),
                 Container(
-                  height: 200,
-                  width: 300,
-                  padding: const EdgeInsets.all(5),
-                  child: InputDecorator(
-                      decoration: InputDecoration(
-                        labelText: 'Opis grupy',
-                        labelStyle: const TextStyle(color: Colors.green),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        )
-                      ),
-                      child: ListView(children: <Widget>[
-                        Text(
-                          _description,
-                        ),
-                        ]
-                      )
-                  )
-                ),
-                const Padding(padding: EdgeInsets.fromLTRB(10, 30, 10, 20)
-                ),
+                    height: 200,
+                    width: 300,
+                    padding: const EdgeInsets.all(5),
+                    child: InputDecorator(
+                        decoration: InputDecoration(
+                            labelText: 'Opis grupy',
+                            labelStyle: const TextStyle(color: Colors.green),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                        child: ListView(children: <Widget>[
+                          Text(
+                            _description,
+                          ),
+                        ]))),
+                const Padding(padding: EdgeInsets.fromLTRB(10, 30, 10, 20)),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
@@ -106,35 +96,40 @@ class _JoinGroup extends State<JoinGroup> {
                         width: 150,
                         padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
                         child: ElevatedButton(
-                          onPressed: _buttonEnabled
-                          ? () async {
-                            _switchButton();
-                            await dao.joinGroup(widget.groupId);
-                            // ignore: use_build_context_synchronously
-                            Navigator.pop(context, "Dołączono do grupy");
-                            _switchButton();
-                          }
-                          : null,
-                          child: const Text("TAK")
-                        )
-                      ),
+                            onPressed: _buttonEnabled
+                                ? () async {
+                                    _switchButton();
+                                    bool flag = true;
+                                    try {
+                                      await dao.joinGroup(widget.groupId);
+                                    } catch (e) {
+                                      flag = false;
+                                      Navigator.pop(
+                                          context, e.toString().substring(10));
+                                    }
+                                    if (flag) {
+                                      // ignore: use_build_context_synchronously
+                                      Navigator.pop(
+                                          context, "Dołączono do grupy");
+                                    }
+                                  }
+                                : null,
+                            child: const Text("TAK"))),
                     Container(
-                      height: 60,
-                      width: 150,
-                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                const Color.fromARGB(255, 255, 0, 0)
-                        ),
-                        onPressed: _buttonEnabled
-                        ? () {
-                          Navigator.pop(context, "Wycofano dołączanie");
-                        }
-                        : null,
-                        child: const Text("NIE")
-                      )
-                    ),
+                        height: 60,
+                        width: 150,
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    const Color.fromARGB(255, 255, 0, 0)),
+                            onPressed: _buttonEnabled
+                                ? () {
+                                    Navigator.pop(
+                                        context, "Wycofano dołączanie");
+                                  }
+                                : null,
+                            child: const Text("NIE"))),
                   ],
                 )
               ],
