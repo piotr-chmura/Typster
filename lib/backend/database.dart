7e2810d5abb4d7d4f3cded011b96f01e1c3cda8d
piotr-chmura <piotr.chmura17@gmail.com>
2023-06-12 00:19:51 +0200

    ip

--- lib/backend/database.dart
+++ lib/backend/database.dart
@@ -2,7 +2,7 @@ import 'package:mysql_client/mysql_client.dart';
 import 'dart:async';
 
 class Mysql {
-  static String host = "217.96.214.67",
+  static String host = "217.96.206.185",
       userName = "Host_PCH_API_UPDATE",
       password = "h>k96{U9M8#2Qr8_",
       databaseName = "typster";
